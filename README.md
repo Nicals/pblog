7390e5a6bb39a5fdb498e6b432295dc5f83daa9a
Nicolas Appriou <nicolas.appriou@gmail.com>
2017-02-26 13:13:11 +0100

    add models

--- README.md
+++ README.md
@@ -13,3 +13,17 @@ file in an **PBLOG_SETTINGS** environment variable.
 | --- | ---- | -------- |
 | DEBUG | bool | defaults to False |
 | SECRET_KEY | string | default to an insecure value |
+| SQLALCHEMY_DATABASE_URI | string | |
+
+
+To build the database:
+
+```python
+from pblog.factory import create_app
+from pblog.core import db
+from pblog.models import *
+
+app = create_app(__name__, '')
+with app.app_context():
+  db.create_all()
+```
---
+++ pblog/models.py
@@ -0,0 +1,27 @@
+from pblog.core import db
+
+
+class Category(db.Model):
+    __tablename__ = 'categories'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True)
+    slug = db.Column(db.String(50))
+
+    def __repr__(self):
+        return '<{} {}:{}>'.format(self.__name__, self.id, self.name)
+
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255))
+    slug = db.Column(db.String(255))
+    summary = db.Column(db.Text())
+    category_id = db.Column(db.Integer, db.ForeignKey('categories.id'))
+    category = db.relationship(
+        'Category', backref=db.backref('posts', lazy='dynamic'))
+
+    def __repr__(self):
+        return '<{} {}:{}>'.format(self.__name__, self.id, self.name)

d46340a9f09c603999006b04ee85eeecb42f15a1
Nicolas Appriou <nicolas.appriou@gmail.com>
2017-03-13 11:55:58 +0100

    add migrations

--- pblog/models.py
+++ pblog/models.py
@@ -5,12 +5,8 @@ class Category(db.Model):
     __tablename__ = 'categories'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), unique=True)
-    slug = db.Column(db.String(50))
-
-    def __init__(self, name, slug):
-        self.name = name
-        self.slug = slug
+    name = db.Column(db.String(50), nullable=False, unique=True)
+    slug = db.Column(db.String(50), nullable=False)
 
     def __str__(self):
         return self.name
@@ -23,25 +19,15 @@ class Post(db.Model):
     __tablename__ = 'posts'
 
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(255))
-    slug = db.Column(db.String(255))
-    summary = db.Column(db.Text())
+    title = db.Column(db.String(255), nullable=False)
+    slug = db.Column(db.String(255), nullable=False)
+    summary = db.Column(db.Text(), default='', nullable=False)
     published_date = db.Column(db.Date(), nullable=False)
-    md_content = db.Column(db.Text())
-    html_content = db.Column(db.Text())
-    category_id = db.Column(db.Integer, db.ForeignKey('categories.id'))
+    md_content = db.Column(db.Text(), nullable=False)
+    html_content = db.Column(db.Text(), nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey('categories.id'), nullable=False)
     category = db.relationship(
         'Category', backref=db.backref('posts', lazy='dynamic'))
-
-    def __init__(self, title, slug, published_date, summary='', md_content='',
-                 html_content='', category=None):
-        self.title = title
-        self.slug = slug
-        self.summary = summary
-        self.published_date = published_date
-        self.md_content = md_content
-        self.html_content = html_content
-        self.category = category
 
     def __repr__(self):
         return '<{} {}:{}>'.format(self.__class__.__name__, self.id, self.name)

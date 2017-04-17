c5c4eb66e979f4bb6e9aa8bcdc0cd61edb863a40
Nicolas Appriou <nicolas.appriou@gmail.com>
2017-04-17 10:16:27 +0200

    use __str__ method for model display

--- flask_pblog/models.py
+++ flask_pblog/models.py
@@ -35,5 +35,8 @@ class Post(Base):
     topic = relationship(
         'Topic', backref=backref('posts', lazy='dynamic'))
 
+    def __str__(self):
+        return self.title
+
     def __repr__(self):
         return '<{} {}:{}>'.format(self.__class__.__name__, self.id, self.title)
